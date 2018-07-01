0e2755275b9d76c3401c946ef46bfababd828cb8
backman <backman.only@gmail.com>
2018-07-01 17:47:28 +0800

    OpNode & NumNode

--- AST/index.js
+++ AST/index.js
@@ -1,10 +1,21 @@
 
-
-
 function Node(){
     this.left=null;
     this.right=null;
-}
+    this.value=null;
+};
 
+// op
+OpNode.prototype = new Node();
+OpNode.prototype.constructor = OpNode();
+function OpNode(value,left,right){
+    this.value=value;
+    this.left=left;
+    this.right=right;
+};
 
+// number
+function NumNode(value){
+    this.value = value;
+};
 
