228de3669a42e254daf41728cec15e6bf9121ee0
backman <backman.only@gmail.com>
2018-07-02 20:55:06 +0800

    add static node.genGraph feature

--- AST/index.js
+++ AST/index.js
@@ -1,8 +1,47 @@
+import fs from 'fs';
+class Node{
 
-function Node(){
-    this.left=null;
-    this.right=null;
-    this.value=null;
+    constructor(){
+        this.left=null;
+        this.right=null;
+        this.value=null;
+    }
+
+    static genGraph(root){
+        var graph ="digraph AST{ \n";
+
+        this.dfs(root,1,(node,idx)=>{
+            var symbolIdx = "n"+idx; 
+            graph+=symbolIdx+" [label=\""+node.value+"\"];\n";
+
+            if(node.left !==null){
+                var leftSymbolIdx = "n"+(2*idx);
+                graph+=symbolIdx+" -> "+leftSymbolIdx+';\n';
+            }
+            if(node.right !==null){
+                var rightSymbolIdx = "n"+(2*idx+1);
+                graph+=symbolIdx+" -> "+rightSymbolIdx+';\n';
+            }
+        });
+        
+        graph+="}";
+        // write to file!
+        fs.writeFile('ASTGraph',graph,  (err)=> {
+            if (err) {
+               return console.error(err);
+            }
+         });
+        return graph; 
+    }
+   
+    static dfs(node,idx,fn){
+
+        if( node === null )
+            return ;
+        fn(node,idx);
+        this.dfs(node.left,2*idx,fn);
+        this.dfs(node.right,2*idx+1,fn);
+    }
 };
 
 // op
@@ -21,4 +60,4 @@ function NumNode(value){
     this.value = value;
 };
 
-export{OpNode,NumNode};
+export{OpNode,NumNode,Node};
--- AST/test/test.js
+++ AST/test/test.js
@@ -1,13 +1,32 @@
 import chai from 'chai';
-import {NumNode} from '../index';
+import {NumNode,OpNode,Node} from '../index';
 let expect = chai.expect;
 
 
 describe("NumNode",()=>{
+    it("genGraph",()=>{
+        //  1+2*3/4
+        var op1 = new OpNode("+",null,null);
+        var op2 = new OpNode("*",null,null);
+        var op3 = new OpNode("/",null,null);
 
-    it("test1",()=>{
-        var t = new NumNode(123);
-        expect(t.value).to.equal(123);
+        var n1 = new NumNode(1);
+        var n2 = new NumNode(2);
+        var n3 = new NumNode(3);
+        var n4 = new NumNode(4);
+
+        //tree structure
+
+        op2.left=op1;
+        op2.right=op3;
+
+        op1.left=n1;
+        op1.right=n2;
+        
+        op3.left=n3;
+        op3.right=n4;
+
+        console.log("\t"+Node.genGraph(op2));
 
 
     });
