b3aabb10d67ab7de0b582eea8864e83b1ff58701
backman <backman.only@gmail.com>
2018-07-10 22:22:39 +0800

    add test case into AST module

--- AST/test/test.js
+++ AST/test/test.js
@@ -1,32 +1,21 @@
 import chai from 'chai';
-import {NumNode,OpNode,Node} from '../index';
+import {NumNode,OpNode,VarNode,AssignNode,StmtNode,ProgramNode,Node} from '../index';
 let expect = chai.expect;
 
 
-describe("NumNode",()=>{
+describe("Program",()=>{
     it("genGraph",()=>{
-        //  1+2*3/4
-        var op1 = new OpNode("+",null,null);
-        var op2 = new OpNode("*",null,null);
-        var op3 = new OpNode("/",null,null);
+        //$a = 1+1;
+        var nNumA = new NumNode(1);
+        var nNumB = new NumNode(1);
 
-        var n1 = new NumNode(1);
-        var n2 = new NumNode(2);
-        var n3 = new NumNode(3);
-        var n4 = new NumNode(4);
+        var nAdd = new OpNode('+',nNumA,nNumB);
+        var nVarA = new VarNode("$a");
+        var nAssign= new AssignNode(nVarA,nAdd);
+        var nStmt = new StmtNode(nAssign);
+        var nProgram = new ProgramNode(nStmt);
 
-        //tree structure
-
-        op2.left=op1;
-        op2.right=op3;
-
-        op1.left=n1;
-        op1.right=n2;
-        
-        op3.left=n3;
-        op3.right=n4;
-
-        console.log("\t"+Node.genGraph(op2));
+        console.log("\t"+Node.genGraph(nProgram));
 
 
     });
